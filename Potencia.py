bb119f102052ee7ffe0e532dca8c8ec8f54a0ca4
Miguel Miranda (Mikee) <117948081+MikeeMP25@users.noreply.github.com>
2022-12-26 13:51:38 -0600

    Update Potencia.py
    agregue una funcion para recorrer una cadena de numeros

--- Potencia.py
+++ Potencia.py
@@ -2,15 +2,17 @@ from typing import List
 
 
 class Potencia:
-
+    digitos=[]
+    
     def __init__(self,valor):
         self.cadena=valor
-        self.a=0
-        self.b=0
-        self.c=0
         
     def RecorrerCadena(self):
-            self.a=int(self.cadena[0])
-            self.b=int(self.cadena[1])
-            self.c=int(str(self.cadena)[-1])
-
+        if self.cadena:
+            for digito in enumerate(cadena):
+                digitos.append(digito)
+        return digitos        
+            
+            
+    def __str__(self):
+        return "El resultado es: "
